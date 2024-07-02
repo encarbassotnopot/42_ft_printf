7e140b7757040b89cdfb6d94c3bd82ec7f10edae
Eina Safor Coma Bages <ecoma-ba@student.42barcelona.com>
2024-07-02 17:56:35 +0200

    i demà més

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,28 @@
 /*   By: ecoma-ba <ecoma-ba@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/30 20:50:27 by ecoma-ba          #+#    #+#             */
-/*   Updated: 2024/06/30 21:55:21 by ecoma-ba         ###   ########.fr       */
+/*   Updated: 2024/07/02 17:55:41 by ecoma-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include <stdarg.h>
 
-int	ft_printf(const char format*, ...)
+int	ft_printf(const char format *, ...)
 {
-	
+	int				count;
+	unsigned char	*str;
+	va_list			ap;
+
+	count = 0;
+	str = (unsigned char *)format;
+	va_start(ap, format);
+	while (str[count])
+	{
+		if (str[count] = '%')
+		{
+			va_arg();
+			handle_flags(str + count, )
+		}
+	}
 }
