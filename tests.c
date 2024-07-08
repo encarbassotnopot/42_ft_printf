e1c717e5b4cf4f818397b959bf57a4bf37d58a82
Eina Safor Coma Bages <ecoma-ba@student.42barcelona.com>
2024-07-08 17:40:02 +0200

    aviam

--- tests.c
+++ tests.c
@@ -6,7 +6,7 @@
 /*   By: ecoma-ba <ecoma-ba@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 15:44:44 by ecoma-ba          #+#    #+#             */
-/*   Updated: 2024/07/08 17:17:40 by ecoma-ba         ###   ########.fr       */
+/*   Updated: 2024/07/08 17:39:53 by ecoma-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,6 @@
 
 int	main(void)
 {
-	ft_printf("%.09s\n", NULL);
-	printf("%.09s\n", NULL);
-	ft_printf("%04d\n", -532);
-	printf("%04d\n", -532);
-	ft_printf("%0+4d\n", 532);
-	printf("%0+4d\n", 532);
-	ft_printf("%04d\n", 5032);
-	printf("%04d\n", 5032);
+	printf("%.03s", "hi low");
+	ft_printf("%.03s", "hi low");
 }
